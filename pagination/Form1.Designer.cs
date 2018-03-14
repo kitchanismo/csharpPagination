f4b46d55ae273f621c12fa021e08f9793cf8083a
kitchanismo <kitchanismo@yahoo.com>
2018-03-14 09:49:51 -0700

    1st

---
+++ pagination/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace pagination
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lvitems = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnlast = new System.Windows.Forms.Button();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.lblpage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(595, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(595, 402);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 31);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lvitems
+            // 
+            this.lvitems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvitems.FullRowSelect = true;
+            this.lvitems.GridLines = true;
+            this.lvitems.Location = new System.Drawing.Point(12, 21);
+            this.lvitems.Name = "lvitems";
+            this.lvitems.Size = new System.Drawing.Size(568, 331);
+            this.lvitems.TabIndex = 3;
+            this.lvitems.UseCompatibleStateImageBehavior = false;
+            this.lvitems.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 68;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "itemname";
+            this.columnHeader2.Width = 127;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "qty";
+            this.columnHeader3.Width = 107;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(423, 363);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(35, 31);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(464, 363);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(35, 31);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(505, 363);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(35, 31);
+            this.btnnext.TabIndex = 6;
+            this.btnnext.Text = ">>";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // btnlast
+            // 
+            this.btnlast.Location = new System.Drawing.Point(546, 363);
+            this.btnlast.Name = "btnlast";
+            this.btnlast.Size = new System.Drawing.Size(35, 31);
+            this.btnlast.TabIndex = 7;
+            this.btnlast.Text = "last";
+            this.btnlast.UseVisualStyleBackColor = true;
+            this.btnlast.Click += new System.EventHandler(this.btnlast_Click);
+            // 
+            // btnprev
+            // 
+            this.btnprev.Enabled = false;
+            this.btnprev.Location = new System.Drawing.Point(380, 363);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(35, 31);
+            this.btnprev.TabIndex = 8;
+            this.btnprev.Text = "<<";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.btnprev_Click);
+            // 
+            // lblpage
+            // 
+            this.lblpage.AutoSize = true;
+            this.lblpage.Location = new System.Drawing.Point(541, 402);
+            this.lblpage.Name = "lblpage";
+            this.lblpage.Size = new System.Drawing.Size(13, 13);
+            this.lblpage.TabIndex = 9;
+            this.lblpage.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 423);
+            this.Controls.Add(this.lblpage);
+            this.Controls.Add(this.btnprev);
+            this.Controls.Add(this.btnlast);
+            this.Controls.Add(this.btnnext);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lvitems);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView lvitems;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnlast;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.Label lblpage;
+    }
+}
+
---
+++ pagination/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace pagination
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loaddata(1, qtyperpage);
+            lblpage.Text = "1 of " + countpage();
+            disablebtn(false);
+        }
+
+        #region variables
+       
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        string query;
+        const int qtyperpage = 10; //number of item per page
+        int first;
+        int second;
+
+        #endregion
+
+        #region buttons
+       
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            loaddata(fromqty(Int32.Parse(btn1.Text)), qtyperpage * Int32.Parse(btn1.Text));
+            lblpage.Text = btn1.Text + " of " + countpage();
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            loaddata(fromqty(Int32.Parse(btn2.Text)), qtyperpage * Int32.Parse(btn2.Text));
+            lblpage.Text = btn2.Text + " of " + countpage();
+            btnprev.Enabled = true;
+        }
+       
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            if (countpage().ToString() == btn2.Text)
+            {
+                return;
+            }
+
+            first = Int32.Parse(btn1.Text) + 1;
+            second = Int32.Parse(btn2.Text) + 1;
+            btn1.Text = first.ToString();
+            btn2.Text = second.ToString();
+            loaddata(fromqty(second), qtyperpage * Int32.Parse(btn2.Text));
+            lblpage.Text = second + " of " + countpage();
+            btnprev.Enabled = true;
+        }
+
+        private void btnprev_Click(object sender, EventArgs e)
+        {
+            if (btn1.Text == "1")
+            {
+                loaddata(1, qtyperpage);
+                lblpage.Text = "1 of " + countpage();
+                btnprev.Enabled = false; 
+                return;
+            }
+            else if (btn1.Text == "2") { btnprev.Enabled = false; }
+
+            first = Int32.Parse(btn1.Text) - 1;
+            second = Int32.Parse(btn2.Text) - 1;
+            btn1.Text = first.ToString();
+            btn2.Text = second.ToString();
+            loaddata(fromqty(second), qtyperpage * Int32.Parse(btn2.Text));
+            lblpage.Text = second + " of " + countpage();
+        }
+        private void btnlast_Click(object sender, EventArgs e)
+        {
+            loaddata(countrows() - (qtyperpage - 1), countrows());
+            first = Int32.Parse(countpage().ToString()) - 1;
+            second = Int32.Parse(countpage().ToString());
+            btn1.Text = first.ToString();
+            btn2.Text = second.ToString();
+            lblpage.Text = countpage() + " of " + countpage();
+            btnprev.Enabled = true;
+        }
+        #endregion
+
+        #region methods/functions
+      
+        void loaddata(int fromqty, int toqty)
+        {
+            con.Close();
+            lvitems.Items.Clear();
+            con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\testdb.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            query = " SELECT * FROM tblitems where id >=  " + fromqty + "   AND id <= " + toqty + " order by id";
+            con.Open();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                ListViewItem with_1 = lvitems.Items.Add((dr["id"]).ToString());
+                with_1.SubItems.Add(dr["item_name"].ToString());
+                with_1.SubItems.Add(dr["qty"].ToString());
+            }
+            con.Close();
+        }
+
+        double countpage()
+        {
+           int res = countrows() / qtyperpage;
+           string str = (Double.Parse(countrows().ToString()) / qtyperpage).ToString();
+    
+           if (str.Contains("."))
+           {
+               res += 1;
+           }
+           return res;
+        }
+       
+        int countrows()
+        {
+            con.Close();
+            int rows = 0;
+            con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\testdb.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            query = "SELECT * FROM tblitems";
+            con.Open();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                rows++;
+            }
+            return rows;
+        }
+       
+  
+        int fromqty(int num)
+        {
+            int j = 1;
+            for (int i = 0; i < num - 1; i++)
+            {
+               j += qtyperpage;    
+            }
+            return j;
+        }
+
+        void disablebtn(bool bl)
+        {
+            if (countrows() < qtyperpage)
+            {
+                btn2.Enabled = bl;
+                btnlast.Enabled = bl;
+                btnnext.Enabled = bl;
+            }
+        }
+
+
+        #endregion
+    }
+}
